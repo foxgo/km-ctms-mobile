9a125b6c0c169919d673c40ef23d1c0ba12e0324
Gary <596765672@qq.com>
2019-07-26 15:53:31 +0800

    添加顶部的tab切换页面功能

--- src/views/healthRecord/index.vue
+++ src/views/healthRecord/index.vue
@@ -1,22 +1,79 @@
 <template>
   <div>
+    <mt-navbar v-model="selected">
+      <mt-tab-item id="1">首页</mt-tab-item>
+      <mt-tab-item id="2">商城</mt-tab-item>
+      <mt-tab-item id="3">数据</mt-tab-item>
+      <mt-tab-item id="4">家庭成员</mt-tab-item>
+    </mt-navbar>
     <health-record-nav />
     <router-view />
   </div>
 </template>
 
 <script>
+import { Navbar, TabItem } from 'mint-ui'
 import HealthRecordNav from './components/HealthRecordNav'
 export default {
   name: 'Index',
-  components: { HealthRecordNav },
+  components: { HealthRecordNav,Navbar,TabItem },
+  data() {
+    return {
+      selected:"3"
+    }
+  },
   mounted() {
     // 修改导航标题
     this.$store.state.app.pageTitle = '健康测量'
+  },
+  watch:{
+    selected(newval,oldval){
+      console.log(newval+"------"+oldval);
+      this.selected = "3"
+      switch (newval) {
+        case '1': 
+          this.$router.push({
+            path:'/'
+          })
+          break;
+        case '2':
+          // this.$router.push({
+          //   path:'/'
+          // })
+          break;
+        case '3': break;
+        case '4':     
+          // this.$router.push({
+          //   path:'/'
+          // })
+          break;
+      }
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    console.log(to);
+    //点击返回按钮后跳到首页
+    // if (Object.keys(to.params).length <= 0) {
+      // this.$router.back('/');
+    // }else{
+      next();
+    // }
   }
 }
 </script>
 
 <style scoped>
+.mint-navbar {
+  position: fixed;
+  top: 40px;
+  z-index: 3;
+  width: 100%;
+  height: 30px;
+  background-color: white;
+}
 
+.mint-navbar .mint-tab-item {
+  padding: 10px 0 0 0;
+  font-size: 18px;
+}
 </style>
